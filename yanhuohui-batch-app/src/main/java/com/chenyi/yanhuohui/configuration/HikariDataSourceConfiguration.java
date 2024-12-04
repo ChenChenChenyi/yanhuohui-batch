812bf26b859a5b4dccec82ee6bceb72c742f4081
chenyi <614362234@qq.com>
2024-12-04 14:14:22 +0800

    FEAT:20241204清除不要的文件，JPA改为Mybatis做准备

---
+++ yanhuohui-batch-app/src/main/java/com/chenyi/yanhuohui/configuration/HikariDataSourceConfiguration.java
@@ -0,0 +1,61 @@
+//package com.chenyi.yanhuohui.configuration;
+//
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//
+//import javax.sql.DataSource;
+//
+///**
+// * @Classname HikariDataSourceConfiguration
+// * @Description TODO
+// * @Date 2024/12/3 19:06
+// * @Created by 陈义
+// */
+//@Configuration
+//public class HikariDataSourceConfiguration {
+//    @Primary
+//    @Bean("primaryDataSourceProperties")
+//    @ConfigurationProperties("spring.datasource.primary")
+//    public DataSourceProperties primaryDataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Primary
+//    @Bean("primaryDataSource")
+//    @Qualifier(value = "primaryDataSource")
+//    // 留意下面这行
+//    @ConfigurationProperties(prefix = "spring.datasource.primary.hikari")
+//    public HikariDataSource primaryDataSource() {
+//        return primaryDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+//    }
+//
+//    @Bean("wangdaiDataSourceProperties")
+//    @ConfigurationProperties("spring.datasource.wangdai")
+//    public DataSourceProperties wangdaiDataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Bean("wangdaiDataSource")
+//    @Qualifier(value = "wangdaiDataSource")
+//    // 留意下面这行
+//    @ConfigurationProperties(prefix = "spring.datasource.wangdai.hikari")
+//    public HikariDataSource wangdaiDataSource() {
+//        return wangdaiDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+//    }
+//
+//    @Bean(name = "primaryJdbcTemplate")
+//    public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") DataSource dataSource) {
+//        return new JdbcTemplate(dataSource);
+//    }
+//
+//    @Bean(name = "wangdaiJdbcTemplate")
+//    public JdbcTemplate wangdaiJdbcTemplate(@Qualifier("wangdaiDataSource") DataSource dataSource) {
+//        return new JdbcTemplate(dataSource);
+//    }
+//}
